bd822664fe39d7a035eeb27eafd1f49ccf83f499
ecao <e.cao@intel.com>
2023-05-16 02:02:08 -0700

    add fp16 support for gemm
    ghstack-source-id: 1a561a85fab31f3725014ff2beaa1228120eedde
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99498

--- aten/src/ATen/native/mkldnn/Matmul.h
+++ aten/src/ATen/native/mkldnn/Matmul.h
@@ -19,6 +19,11 @@ bool use_mkldnn_bf16_matmul(
     const Tensor& mat2,
     const Tensor& result_opt);
 
+bool use_mkldnn_fp16_matmul(
+    const Tensor& mat1,
+    const Tensor& mat2,
+    const Tensor& result_opt);
+
 // Try running mkldnn optimized gemm, or returns false if naive gemm would be faster
 bool mkldnn_bf16_gemm(
     TransposeType transa, TransposeType transb,
@@ -28,6 +33,11 @@ bool mkldnn_bf16_gemm(
     const c10::BFloat16 *b, int64_t ldb,
     float beta,
     c10::BFloat16 *c, int64_t ldc);
+
+bool use_mkldnn_lower_precision_matmul(
+    const Tensor& mat1,
+    const Tensor& mat2,
+    const Tensor& result);
 
 }
 
