be39ac2796e4ba790474dde78ffe0d464d7276de
Peter Bell <peterbell10@live.co.uk>
2023-05-16 17:09:04 +0100

    [inductor] Constant and index_expr propagation pass
    This pass does a limited for of constant propagation, as well as propagation of
    sympy indexing expressions. For example, say you have the function:
    ```python
    def flip(x):
        i = torch.arange(x.size(0) - 1, -1, -1, device=x.device)
        return x[i]
    ```
    
    On current main this results in indirect indexing:
    ```python
    class buf0_loop_body:
        var_ranges = {z0: 4, z1: 3}
        index0 = 3 - z0
        index1 = 3*indirect0 + z1
        index2 = 3*z0 + z1
        def body(self, ops):
            get_index = self.get_index('index0')
            index_expr = ops.index_expr(get_index, torch.int64)
            set_indirect0 = self.set_indirect0(index_expr)
            get_index_1 = self.get_index('index1')
            load = ops.load('arg0_1', get_index_1)
            get_index_2 = self.get_index('index2')
            store = ops.store('buf0', get_index_2, load, None)
            return store
    ```
    
    With this PR the indexing is propagated through the computation and into direct
    indexing:
    
    ```python
    class buf0_loop_body:
        var_ranges = {z0: 4, z1: 3}
        index0 = -3*z0 + z1 + 9
        index1 = 3*z0 + z1
        def body(self, ops):
            get_index = self.get_index('index0')
            load = ops.load('arg0_1', get_index)
            get_index_1 = self.get_index('index1')
            store = ops.store('buf0', get_index_1, load, None)
            return store
    ```
    
    ghstack-source-id: 980bee54f9cbc266d1ccd1fbe00a1ca68b190799
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101077

---
+++ torch/_inductor/index_propagation.py
@@ -0,0 +1,167 @@
+"""
+This file implements the IndexPropagation ops handler, which wraps an
+underlying handler to add a limited for of constant propagation, as well as
+propagation of sympy expressions downstream of `ops.index_expr` calls.
+
+"""
+import itertools
+from dataclasses import dataclass
+from typing import Any, Optional
+
+import sympy
+
+import torch
+from torch._prims_common import is_boolean_dtype, is_integer_dtype
+
+
+@dataclass
+class TypedExpr:
+    """A SymPy expression with associated type"""
+
+    expr: sympy.Expr
+    dtype: torch.dtype
+
+
+class SymPyOps:
+    """An ops handler where all IR values are SymPy expressions
+
+    When a value cannot be represented as a SymPy expression, the method is
+    either not defined, or returns NotImplemented
+
+    """
+
+    @staticmethod
+    def identity(value):
+        return value
+
+    @staticmethod
+    def constant(value, dtype):
+        if is_boolean_dtype(dtype):
+            expr = sympy.Integer(bool(value))
+        elif is_integer_dtype(dtype):
+            expr = sympy.Integer(int(value))
+        else:
+            expr = sympy.Float(float(value))
+        return TypedExpr(expr, dtype)
+
+    @staticmethod
+    def index_expr(value, dtype):
+        if isinstance(value, int):
+            value = sympy.Integer(value)
+        return TypedExpr(value, dtype)
+
+    @staticmethod
+    def to_dtype(value, dtype):
+        if isinstance(value.expr, (sympy.Integer, sympy.Float)):
+            return SymPyOps.constant(value.expr, dtype)
+        elif is_integer_dtype(dtype) and is_integer_dtype(value.dtype):
+            return SymPyOps.index_expr(value.expr, dtype)
+        else:
+            # TODO: Inductor doesn't handle floating point in sympy expressions well at the moment
+            return NotImplemented
+
+    @staticmethod
+    def square(x):
+        return TypedExpr(x.expr * x.expr, x.dtype)
+
+    @staticmethod
+    def add(x, y):
+        result_type = torch.promote_types(x.dtype, y.dtype)
+        return TypedExpr(x.expr + y.expr, result_type)
+
+    @staticmethod
+    def sub(x, y):
+        result_type = torch.promote_types(x.dtype, y.dtype)
+        return TypedExpr(x.expr - y.expr, result_type)
+
+    @staticmethod
+    def mul(x, y):
+        result_type = torch.promote_types(x.dtype, y.dtype)
+        return TypedExpr(x.expr * y.expr, result_type)
+
+    @staticmethod
+    def neg(x):
+        return TypedExpr(-x.expr, x.dtype)
+
+
+@dataclass
+class IndexPropVar:
+    rep: Any  # A wrapped IR value, if present
+    expr: Optional[TypedExpr] = None
+
+
+class IndexPropagation:
+    """Ops wrapper that tries to propagate constant and index_expr values through the computation.
+
+    This aims to maximize the compile time simplification possible, and convert
+    indirect indexing from arange into normal static indexing.
+
+    """
+
+    def __init__(self, inner):
+        self._inner = inner
+        self.id_iter = itertools.count()
+
+    def new_symbol(self, expr: TypedExpr):
+        return IndexPropVar(f"index_tmp{next(self.id_iter)}", expr)
+
+    def materialize_expr(self, expr, dtype):
+        # Construct a new constant/index_expr from the SymPy expression
+        if isinstance(expr, sympy.Integer):
+            return self._inner.constant(int(expr), dtype)
+        elif not expr.free_symbols:
+            return self._inner.constant(float(expr), dtype)
+        return self._inner.index_expr(expr, dtype)
+
+    def unwrap(self, a):
+        if not isinstance(a, IndexPropVar):
+            return a
+
+        # Prefer the sympy representation if possible
+        if a.expr is not None:
+            return self.materialize_expr(a.expr.expr, a.expr.dtype)
+
+        return a.rep
+
+    def fallback(self, name, args, kwargs):
+        # Fallback to the wrapped handler
+        new_args = [self.unwrap(a) for a in args]
+        new_kwargs = {k: self.unwrap(v) for k, v in kwargs.items()}
+        return IndexPropVar(getattr(self._inner, name)(*new_args, **new_kwargs))
+
+    def propagate_sympy(self, name, args, kwargs):
+        # Builld a new SymPy expression from this ops call
+        def unwrap(a):
+            if not isinstance(a, IndexPropVar):
+                return a
+            return a.expr
+
+        new_args = [unwrap(a) for a in args]
+        new_kwargs = {k: unwrap(v) for k, v in kwargs.items()}
+        new_expr = getattr(SymPyOps, name)(*new_args, **new_kwargs)
+        if new_expr is NotImplemented:
+            return self.fallback(name, args, kwargs)
+        return self.new_symbol(new_expr)
+
+    def __getattr__(self, name):
+        def inner(*args, **kwargs):
+            if not hasattr(SymPyOps, name):
+                return self.fallback(name, args, kwargs)
+
+            var_arguments = [
+                a
+                for a in itertools.chain(args, kwargs.values())
+                if isinstance(a, IndexPropVar)
+            ]
+            if any(v.expr is None for v in var_arguments):
+                return self.fallback(name, args, kwargs)
+
+            return self.propagate_sympy(name, args, kwargs)
+
+        return inner
+
+    def indirect_indexing(self, index, size):
+        # indirect_indexing returns a sympy value, so no need to wrap in IndexPropVar here
+        if isinstance(index, IndexPropVar) and index.expr is not None:
+            return index.expr.expr
+        return self.fallback("indirect_indexing", (index, size), {}).rep
