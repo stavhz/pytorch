eac17b9908f58b11d8193940d5c1608353c562be
Edward Z. Yang <ezyang@meta.com>
2023-05-16 11:04:55 -0700

    Fix shape mismatch in vision_maskrcnn
    The bulk of the heavy lifting is happening in
    https://github.com/pytorch/vision/pull/7592
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    ghstack-source-id: 28e7047b3ed1058cf3e8009cd29e7146cacc4426
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101477

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -97,7 +97,6 @@ CI_SKIP[CI("eager", training=True)] = [
     "hf_BigBird",  # fp64_OOM
     "hf_T5_base",  # fp64_OOM
     "llama",  # Accuracy failed: allclose not within tol=0.001
-    "vision_maskrcnn",  # shape mismatch in accuracy check!
     # Huggingface
     "XGLMForCausalLM",  # OOM
     # TIMM
@@ -143,7 +142,6 @@ CI_SKIP[CI("aot_eager", training=True)] = [
     "resnet50_quantized_qat",  # fp64_OOM
     "moco",
     "pytorch_struct",
-    "vision_masrkcnn",  # AssertionError: nothing in example_inputs had a dim with 4
     # Huggingface
     "MBartForConditionalGeneration",  # OOM
     "M2M100ForConditionalGeneration",  # OOM
@@ -254,7 +252,7 @@ CI_SKIP[CI("aot_eager", training=False, dynamic=True)] = [
     *CI_SKIP[CI("aot_eager", training=False)],
     "cm3leon_generate",  # Could not validate constraint UnspecConstraint
     "hf_T5_generate",  # Could not validate constraint UnspecConstraint
-    "vision_maskrcnn",  # nothing in example_inputs had a dim with 4
+    "vision_maskrcnn",  # accuracy failure on boxes, after https://github.com/pytorch/pytorch/issues/101093
 ]
 
 CI_SKIP[CI("aot_eager", training=True, dynamic=True)] = [
